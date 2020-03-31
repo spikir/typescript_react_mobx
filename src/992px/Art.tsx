77056729b35fea52de9862bed240471477bde737
spikir <spikirard@gmail.com>
2020-03-31 19:28:40 +0200

    Add files via upload

--- src/992px/Art.tsx
+++ src/992px/Art.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom"
 import parse from 'html-react-parser';
 import { observer } from "mobx-react"
 import { AppStore } from "../AppStore/AppStore"
+import Scroll from "./Scroll"
 
 
 interface IProps {
@@ -13,27 +14,28 @@ interface IProps {
 interface IState {}
 
 @observer
-export class Art992 extends React.Component<IProps, IState> {
+export class Art992 extends React.Component<IProps, IState>  {
+
     public render() {
         const appStore = this.props.appStore;
-        let list: string;
-        list = '';
+        let list: string = '';
         appStore.firstSixArt.model.map((entry, index) => {
             if(index == 0) {
-                list += '<div className="containerMainPic">';
-                    list += '<img className="mainPicArt" src='+entry.art_link+' />';
+                const fixScroll: string = appStore.classFixScroll.model.find(element => 'MainPic');
+                list += '<div class="containerMainPic '+fixScroll+'">';
+                    list += '<img class="mainPicArt" src='+entry.art_link+' />';
                 list += '</div>';
-                list += '<div className="listArt">'; //introduce class listArt
-                list += '<div className="row">'; //introduce class row
+                list += '<div class="listArt">'; //introduce class listArt
+                list += '<div class="row">'; //introduce class row
             } else {
-                list += '<div className="article">'; //introduce class article
-                    list += '<div className="articleImg">'; //introduce class articleImg
-                        list += '<img className="listImg" src='+entry.art_link+' />';
-				        list += '<div className="overlay"></div>';
+                list += '<div class="article">'; //introduce class article
+                    list += '<div class="articleImg">'; //introduce class articleImg
+                        list += '<img class="listImg" src='+entry.art_link+' />';
+				        list += '<div class="overlay"></div>';
                     list += '</div>'; //Finish class articleImg
-                    list += '<div className="articleText">'; //Introduce class articleText
-                        list += '<div className="articleTextTitle">'+entry.art_title+'</div>';
-                        list += '<div className="articleTextText">'+entry.art_desc+'</div>';
+                    list += '<div class="articleText">'; //Introduce class articleText
+                        list += '<div class="articleTextTitle">'+entry.art_title+'</div>';
+                        list += '<div class="articleTextText">'+entry.art_desc+'</div>';
                     list += '</div>'; //Finish class articleText
                 list += '</div>'; //Finish class article
             } 
