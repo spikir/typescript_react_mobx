cf73f65d62f7a40f0cb3064d098d104df3aa8772
spikir <spikirard@gmail.com>
2020-06-20 19:06:47 +0200

    Added hamburger menu

--- src/inc/Header.tsx
+++ src/inc/Header.tsx
@@ -2,16 +2,49 @@ import React from 'react'
 import {
     Link
   } from "react-router-dom"
+import { HamburgerButton } from 'react-hamburger-button';
 
-export default class Header extends React.Component {
+interface State {
+    open?: boolean;
+}
+
+export default class Header extends React.Component<{}, State> {
+    private classNavi:boolean = false;
+    
+    constructor(props) {
+        super(props);
+    }
+
+    public readonly state: State = {
+		open: false
+	};
+
+    handleClick() {
+        this.classNavi = !this.classNavi;
+        this.setState({
+            open: !this.state.open
+        });
+    }
+
     render() {
         return (
-            <div>
+            <React.Fragment>
                 <div className="header">
                     Nadege
                 </div>
                 <div className="navi">
-                    <div className="naviWrap">
+                    <div className="btnBurger">
+                    <HamburgerButton
+                        open={this.state.open}
+                        onClick={this.handleClick.bind(this)}
+                        width={18}
+                        height={15}
+                        strokeWidth={1}
+                        color='black'
+                        animationDuration={0.5}
+                    />
+                    </div>
+                    <div className={this.classNavi ? 'naviWrapShow' : 'naviWrap naviWrapHidden'}>
                         <div className="naviRow">
                         <div className="naviRowOverlay"></div>
                         <Link to="/">Home</Link>
@@ -38,7 +71,7 @@ export default class Header extends React.Component {
                         </div>
                     </div>
                 </div>
-            </div>
+            </React.Fragment>
         );
     };
 }
